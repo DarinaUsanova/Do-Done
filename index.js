a58653832a57b205035b286da610342619c4af0e
Darina Usanova <darinushkaaaa@gmail.com>
2023-12-18 16:07:00 +0100

    feat: add js

---
+++ index.js
@@ -0,0 +1,40 @@
+const input = document.querySelector('input');
+const button = document.querySelector('.add-button');
+const ul = document.querySelector('ul');
+
+function addTask() {
+  if(input.value === '') {
+    alert('Please enter something)!');
+  }
+  else {
+    let li = document.createElement('li');
+    li.innerHTML = input.value;
+    ul.appendChild(li);
+    input.value = '';
+
+    let starSpan = document.createElement('span');
+    starSpan.innerHTML = '<i class="fa-solid fa-star"></i>';
+    li.prepend(starSpan);
+    let delSpan = document.createElement('span');
+    delSpan.innerHTML = '<i class="fa-solid fa-eraser"></i>';
+    li.append(delSpan);
+  }
+  saveData();
+}
+ul.addEventListener('click', function(e) {
+  if (e.target.closest('.fa-eraser')) {
+    e.target.closest('li').remove();
+    saveData();
+  } else if (e.target.closest('li')) {
+    e.target.classList.toggle('checked');
+    saveData();
+  }
+});
+
+function saveData() {
+  localStorage.setItem('data', ul.innerHTML);
+}
+function showData(){
+  ul.innerHTML = localStorage.getItem('data');
+}
+showData();
